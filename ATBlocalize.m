d9eb4ddfe3594f1cdadace568fffdc6fd1ee7e6a
Pietro <piesinopoli@gmail.com>
2016-09-22 14:34:50 +0200

    Add files via upload

---
+++ ATBlocalize.m
@@ -0,0 +1,16 @@
+function ATBlocalize()
+    
+    global coor
+    
+    coor = zeros(3,2);
+    
+    coor(1,1) = -2;
+    coor(1,2) = 0;
+    coor(1,3) = 5;
+    
+    coor(2,1) = 4;
+    coor(2,2) = 0;
+    coor(2,3) = 4;
+
+end
+
