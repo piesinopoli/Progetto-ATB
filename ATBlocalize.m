960901106a7880ba9ba9225ce795c4930b1f46cf
Pietro <piesinopoli@gmail.com>
2016-10-07 23:01:41 +0200

    Add files via upload

--- ATBlocalize.m
+++ ATBlocalize.m
@@ -2,9 +2,9 @@ function ATBlocalize()
     
     coor = zeros(2,3);
     
-    coor(1,1) = -4;
+    coor(1,1) = -1;
     coor(1,2) = 0;
-    coor(1,3) = 6;
+    coor(1,3) = 9;
     
     coor(2,1) = 7;
     coor(2,2) = 0;
--- ATBobst.m
+++ ATBobst.m
@@ -1,145 +1,259 @@
 function ATBobst(coor)
-    
+
     distance = zeros(1,10);                                                             %creo l'array che conterrà la distanza che il robot deve percorrere
     angolo = zeros(1,10);                                                               %creo l'array che conterrà l'angolo di svolta del robot
     mode = zeros(1,5);                                                                  %creo l'array che conterrà il senso di svolta del robot
     
-    simulation = zeros(50,50);                                                          %creo la matrice che conterra la simulazione del tutto
+    simulation = zeros(100,100);                                                        %creo la matrice che conterra la simulazione del tutto
     
-    for i=1:1:50
+    for i=1:1:100
         simulation(1,i) = 128;
-        for n=1:1:50
+        for n=1:1:100
             simulation(i,n) = 128;
         end
     end
     
-    marginLeftX = coor(1,1)*1;                                                          %trovo la distanza del punto sinistro sull'asse delle x
-    marginLeftY = coor(2,1)*1;                                                          %trovo la distanza del punto sinistro sull'asse delle y
-    marginRightX = coor(1,3)*1;                                                         %trovo la distanza del punto destro sull'asse delle x
-    marginRightY = coor(2,3)*1;                                                         %trovo la distanza del punto destro sull'asse delle y
+    rapportoPixel = 2;                                                                          
+    rapportoCentimetri = 1;
     
-    pixel = 5;                                                                          %rapporo lunghezza pixel
+    marginLeftX = abs(coor(1,1));
+    marginLeftY = abs(coor(2,1));
+    marginRightX = abs(coor(1,3));
+    marginRightY = abs(coor(2,3));
     
-    l1 = abs(marginLeftX)*pixel;                                                        %trasformo le coordinate in pixel del margine sinistro sull'asse delle x
-    l2 = abs(marginLeftY)*pixel;                                                        %trasformo le coordinate in pixel del margine sinistro sull'asse delle y
-    r1 = abs(marginRightX)*pixel;                                                       %trasformo le coordinate in pixel del margine destro sull'asse delle x
-    r2 = abs(marginRightY)*pixel;                                                       %trasformo le coordinate in pixel del margine destro sull'asse delle y
+    realMarginLeftX = coor(1,1)*rapportoCentimetri;
+    realMarginLeftY = coor(2,1)*rapportoCentimetri;
+    realMarginRightX = coor(1,3)*rapportoCentimetri;
+    realMarginRightY = coor(2,3)*rapportoCentimetri;
     
+    simulationDistanza = (abs(coor(1,3)-coor(1,1)))*rapportoPixel;
+    
+    simulationMarginLeftX = int64(50-(simulationDistanza/2));
+    simulationMarginLeftY = 50;
+    simulationMarginRightX = 100-(100-simulationDistanza-simulationMarginLeftX);
+    simulationMarginRightY = 50;
+ 
     %%%CREAZIONE GUI%%%
     figure1 = figure('Color',[1 0.3 0.4],'MenuBar','none','Name','Simulazione','Resize','on','NumberTitle','off');
     btn1 = uicontrol('Style', 'pushbutton', 'String', {'ESCI'},'Position',[460 20 80 40],'Callback', @ATBclose);
     subplot(1,2,1);
     %%%CREAZIONE GUI%%%
     
-    simulation(int64(l2),int64(l1)) = 0;                                                %abbino i margini dell'oggetto alla matrice della simulazione
-    simulation(int64(r2),int64(r1)) = 0;
-    
-    if (abs(marginLeftX) < abs(marginRightX))                                           %scelgo se il robot deve girare a destra o sinistra vedendo quale punto è più corto
+    simulation(simulationMarginLeftY,simulationMarginLeftX) = 0;                                                        
+    simulation(simulationMarginRightY,simulationMarginRightX) = 0;
+
+    if (abs(marginLeftX) < abs(marginRightX))                                                                            
+                   
+        realIpotenusa = sqrt(((realMarginLeftX)^2)+((realMarginLeftY)^2));                                                  
+        simulationIpotenusa = (sqrt(((realMarginLeftX)^2)+((realMarginLeftY)^2)))*rapportoPixel
         
-        ipotenusa = sqrt(((marginLeftX + 5)^2) + ((marginLeftY + 5)^2));                %trovo la distanza che deve percorrere il robot
-        alpha = acos(ipotenusa/marginLeftY);                                            %trovo l'angolo di svolta del robot
+        alpha = 90-((acos(realIpotenusa/realMarginLeftY))+10);
         
-        simulationSvolta = sqrt(((marginRightX)^2) + (marginRightY)^2);                 %trovo la distanza che deve percorrere il robot nell simulazione
-        
-        s2 = l2 + 10;                                                                   %trovo le coordinate del punto sul robot
-        s1 = l1 + 1;                                                                    
-        
-        simulation(s2,s1) = 0;                                                          %abbino le coordinate alla matrice della simulazione
-                                  
-        %%%PRIMO MOVIMENTO%%%
-        distance(1) = ipotenusa;                                                        %distanza che il robot deve percorrere
-        angolo(1) = alpha;                                                              %angolo di svolta del robot
-        mode(1) = 2;                                                                    %modo in cui deve girare il robot
-        ATBmove(mode, angolo, distance);                                                %restituisco alla funzione di movimento gli array
-        %%%PRIMO MOVIMENTO%%%
+        if (coor(1,1) < 0)
+            
+            s1=(simulationMarginLeftX)+(marginLeftX*rapportoPixel);
+            s2=(simulationMarginLeftY)+(marginLeftY*rapportoPixel);
+            
+            simulation(s2,s1) = 0;
+            
+            %%%PRIMO MOVIMENTO%%%
+            distance(1) = realIpotenusa;                                                       
+            angolo(1) = alpha;                                                              
+            mode(1) = 2;                                                                   
+            ATBmove(mode, angolo, distance);                                                
+            %%%PRIMO MOVIMENTO%%%
     
-        for c=1:1:simulationSvolta                                                      %tutto questo per quanto riguarda la simulazione
-            cam1=s1-c;                                                                  
-            cam2=s2-c;
-            simulation(cam2,cam1) = 0;
-            subimage(simulation);
-            pause(0.3);
-        end
+            for c=1:1:simulationIpotenusa                                                      
+                cam1=s1-c;                                                                  
+                cam2=s2-c;
+                simulation(cam2,cam1) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
     
-        %%%SECONDO MOVIMENTO%%% 
-        distance(1) = 15;                                                               %distanza indicativa
-        angolo(1) = alpha;                                                              %angolo di svolta del robot       
-        mode(1) = 1;                                                                    %modo in cui deve girare il robot
-        ATBmove(mode, angolo, distance);                                                %restituisco alla funzione di movimento gli array
-        %%%SECONDO MOVIMENTO%%%
+            %%%SECONDO MOVIMENTO%%% 
+            distance(1) = 15;                                                               
+            angolo(1) = alpha;                                                                     
+            mode(1) = 1;                                                                    
+            ATBmove(mode, angolo, distance);                                                
+            %%%SECONDO MOVIMENTO%%%
     
-        for d=1:1:15                                                                    %tutto questo per quanto riguarda la simulazione
-            cam3=cam2-d;
-            simulation(cam3,cam1) = 0;
-            subimage(simulation);
-            pause(0.3);
-        end
+            for d=1:1:15                                                                    
+                cam3=cam2-d;
+                simulation(cam3,cam1) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
     
-        %%%TERZO MOVIMENTO%%%
-        distance(1) = ipotenusa;                                                        %distanza che il robot deve percorrere
-        angolo(1) = alpha;                                                              %angolo di svolta del robot
-        mode(1) = 1;                                                                    %modo in cui deve girare il robot
-        ATBmove(mode, angolo, distance);                                                %restituisco alla funzione di movimento gli array
-        %%%TERZO MOVIMENTO%%%
+            %%%TERZO MOVIMENTO%%%
+            distance(1) = realIpotenusa;                                                        
+            angolo(1) = alpha;                                                              
+            mode(1) = 1;                                                                    
+            ATBmove(mode, angolo, distance);                                                
+            %%%TERZO MOVIMENTO%%%
        
-        for e=1:1:simulationSvolta                                                      %tutto questo per quanto riguarda la simulazione
-            simulation(cam3 - e,cam1 + e) = 0;
-            subimage(simulation);
-            pause(0.3);
-        end
+            for e=1:1:simulationIpotenusa                                                      
+                simulation(cam3 - e,cam1 + e) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
+            
+        else
+                
+            s1=(simulationMarginLeftX)-(marginLeftX*rapportoPixel);
+            s2=(simulationMarginLeftY)+(marginLeftY*rapportoPixel);
+            
+            simulation(s2,s1) = 0;
+            
+            %%%PRIMO MOVIMENTO%%%
+            distance(1) = realIpotenusa;                                                        
+            angolo(1) = alpha;                                                              
+            mode(1) = 2;                                                                    
+            ATBmove(mode, angolo, distance);                                                
+            %%%PRIMO MOVIMENTO%%%
+    
+            for c=1:1:simulationIpotenusa                                                     
+                cam1=s1-c;                                                                  
+                cam2=s2-c;
+                simulation(cam2,cam1) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
+    
+            %%%SECONDO MOVIMENTO%%% 
+            distance(1) = 15;                                                               
+            angolo(1) = alpha;                                                                    
+            mode(1) = 1;                                                                    
+            ATBmove(mode, angolo, distance);                                                
+            %%%SECONDO MOVIMENTO%%%
+    
+            for d=1:1:15                                                                    
+                cam3=cam2-d;
+                simulation(cam3,cam1) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
+    
+            %%%TERZO MOVIMENTO%%%
+            distance(1) = realIpotenusa;                                                        
+            angolo(1) = alpha;                                                              
+            mode(1) = 1;                                                                   
+            ATBmove(mode, angolo, distance);                                                
+            %%%TERZO MOVIMENTO%%%
+       
+            for e=1:1:simulationIpotenusa                                                     
+                simulation(cam3 - e,cam1 + e) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end            
+            
+        end 
         
     else
     
-        ipotenusa = sqrt(((marginRightX + 5)^2) + (marginRightY + 5)^2);  
-        alpha = acos(ipotenusa/marginRightY);                           
+        realIpotenusa = sqrt(((realMarginRightX)^2)+((realMarginRightY)^2));                
+        simulationIpotenusa = (sqrt(((realMarginRightX)^2)+((realMarginRightY)^2)))*rapportoPixel
         
-        simulationSvolta = sqrt(((marginRightX)^2) + (marginRightY)^2);
+        alpha = (acos(realIpotenusa/realMarginRightY))+10;
         
-        s1 = r1 + 5;
-        s2 = r2 + 10;
+        if (coor(1,3) < 0)
+            
+            s1=(simulationMarginRightX)+(marginRightX*rapportoPixel);
+            s2=(simulationMarginRightY)+(marginRightY*rapportoPixel);
+            
+            simulation(s2,s1) = 0;
+            
+            %%%PRIMO MOVIMENTO%%%
+            distance(1) = realIpotenusa;
+            angolo(1) = alpha;
+            mode(1) = 1;
+            ATBmove(mode, angolo, distance);
+            %%%PRIMO MOVIMENTO%%%
         
-        simulation(s2,s1) = 0;
-        
-        %%%PRIMO MOVIMENTO%%%
-        distance(1) = ipotenusa;
-        angolo(1) = alpha;
-        mode(1) = 1;
-        ATBmove(mode, angolo, distance);
-        %%%PRIMO MOVIMENTO%%%
-        
-        for c=1:1:simulationSvolta
-            cam1=s1+c;
-            cam2=s2-c;
-            simulation(cam2,cam1) = 0;
-            subimage(simulation);
-            pause(0.3);
-        end
+            for c=1:1:simulationIpotenusa
+                cam1=s1+c;
+                cam2=s2-c;
+                simulation(cam2,cam1) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
     
-        %%%SECONDO MOVIMENTO%%%
-        distance(1) = 15; 
-        angolo(1) = alpha;
-        mode(1) = 2;
-        ATBmove(mode, angolo, distance);    
-        %%%SECONDO MOVIMENTO%%%
+            %%%SECONDO MOVIMENTO%%%
+            distance(1) = 15; 
+            angolo(1) = alpha;
+            mode(1) = 2;
+            ATBmove(mode, angolo, distance);    
+            %%%SECONDO MOVIMENTO%%%
         
-        for d=1:1:15
-            cam3=cam2-d;
-            simulation(cam3,cam1) = 0;
-            subimage(simulation);
-            pause(0.3);
-        end
+            for d=1:1:15
+                cam3=cam2-d;
+                simulation(cam3,cam1) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
     
-        %%%TERZO MOVIMENTO%%%
-        distance(1) = ipotenusa;
-        angolo(1) = alpha;
-        mode(1) = 2;
-        ATBmove(mode, angolo, distance);     
-        %%%TERZO MOVIMENTO%%%
+            %%%TERZO MOVIMENTO%%%
+            distance(1) = realIpotenusa;
+            angolo(1) = alpha;
+            mode(1) = 2;
+            ATBmove(mode, angolo, distance);     
+            %%%TERZO MOVIMENTO%%%
         
-        for e=1:1:simulationSvolta
-            simulation(cam3 - e,cam1 - e) = 0;
-            subimage(simulation);
-            pause(0.3);
+            for e=1:1:simulationIpotenusa
+                simulation(cam3 - e,cam1 - e) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
+            
+        else
+            
+            s1=(simulationMarginRightX)-(marginRightX*rapportoPixel);
+            s2=(simulationMarginRightY)+(marginRightY*rapportoPixel);
+            
+            simulation(s2,s1) = 0;
+            
+            %%%PRIMO MOVIMENTO%%%
+            distance(1) = realIpotenusa;
+            angolo(1) = alpha;
+            mode(1) = 1;
+            ATBmove(mode, angolo, distance);
+            %%%PRIMO MOVIMENTO%%%
+        
+            for c=1:1:simulationIpotenusa
+                cam1=s1+c;
+                cam2=s2-c;
+                simulation(cam2,cam1) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
+    
+            %%%SECONDO MOVIMENTO%%%
+            distance(1) = 15; 
+            angolo(1) = alpha;
+            mode(1) = 2;
+            ATBmove(mode, angolo, distance);    
+            %%%SECONDO MOVIMENTO%%%
+        
+            for d=1:1:15
+                cam3=cam2-d;
+                simulation(cam3,cam1) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
+    
+            %%%TERZO MOVIMENTO%%%
+            distance(1) = realIpotenusa;
+            angolo(1) = alpha;
+            mode(1) = 2;
+            ATBmove(mode, angolo, distance);     
+            %%%TERZO MOVIMENTO%%%
+        
+            for e=1:1:simulationIpotenusa
+                simulation(cam3 - e,cam1 - e) = 0;
+                subimage(simulation);
+                pause(0.3);
+            end
+            
         end
 
     end
